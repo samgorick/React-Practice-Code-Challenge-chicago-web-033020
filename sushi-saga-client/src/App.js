efe90eb418d001b54eef42c4d01f8d0102c8d756
gozmeister <samgorick.sg@gmail.com>
2020-06-09 15:14:39 -0500

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,72 +1,68 @@
-import React, { Component } from 'react';
-import SushiContainer from './containers/SushiContainer';
-import Table from './containers/Table';
-import SushiWallet from './components/SushiWallet'
+import React, { Component } from "react";
+import SushiContainer from "./containers/SushiContainer";
+import Table from "./containers/Table";
+import SushiWallet from "./components/SushiWallet";
 
 // Endpoint!
-const API = "http://localhost:3000/sushis"
+const API = "http://localhost:3000/sushis";
 
 class App extends Component {
-  constructor(){
-    super()
+  constructor() {
+    super();
     this.state = {
       allSushi: [],
       showSushi: [],
       eatenSushi: [],
       index: 4,
-      dollar: 200
-    }
+      dollar: 200,
+    };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     fetch(API)
-    .then(resp => resp.json())
-    .then(sushiData => {
-      // const updatedSushi = sushiData.map(sushi => ({...sushi, eaten: false}))
-      this.setState({
-        allSushi: sushiData,
-        showSushi: sushiData.slice(0, 4)
-      })
-    })
+      .then((resp) => resp.json())
+      .then((sushiData) => {
+        // const updatedSushi = sushiData.map(sushi => ({...sushi, eaten: false}))
+        this.setState({
+          allSushi: sushiData,
+          showSushi: sushiData.slice(0, 4),
+        });
+      });
   }
 
   showMoreSushi = () => {
-    console.log(this.state.index)
     this.setState((prevState) => ({
-      showSushi: prevState.allSushi.slice((prevState.index), (prevState.index + 4)),
-      index: prevState.index > 95 ? 0 : prevState.index + 4
-    }))
-  }
+      showSushi: prevState.allSushi.slice(prevState.index, prevState.index + 4),
+      index: prevState.index + 4 > prevState.allSushi.length ? 0 : prevState.index + 4,
+    }));
+  };
 
   plateClicked = (sushi) => {
-    if(this.state.dollar > sushi.price) {
+    if (this.state.dollar > sushi.price) {
       this.setState((prevState) => ({
         eatenSushi: [...prevState.eatenSushi, sushi],
-        dollar: prevState.dollar - sushi.price
-      }))
+        dollar: prevState.dollar - sushi.price,
+      }));
     }
-  }
+  };
 
   handleForm = (amount) => {
     this.setState((prevState) => ({
-      dollar: prevState.dollar + parseInt(amount, 10)
-    }))
-  } 
+      dollar: prevState.dollar + parseInt(amount, 10),
+    }));
+  };
 
   render() {
     return (
       <div className="app">
-        <SushiContainer 
+        <SushiContainer
           sushi={this.state.showSushi}
-          eatenSushi={this.state.eatenSushi} 
+          eatenSushi={this.state.eatenSushi}
           showMoreSushi={this.showMoreSushi}
           plateClicked={this.plateClicked}
         />
-        <Table 
-          plates={this.state.eatenSushi}
-          dollar={this.state.dollar}
-        />
-        <SushiWallet handleForm={this.handleForm}/>
+        <Table plates={this.state.eatenSushi} dollar={this.state.dollar} />
+        <SushiWallet handleForm={this.handleForm} />
       </div>
     );
   }
